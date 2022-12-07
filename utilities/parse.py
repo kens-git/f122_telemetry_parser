c91fa0bf03f2906de596cbb984fe083afcc277ef
Ken Montgomery <ID+username@users.noreply.github.com>
2022-12-07 17:50:46 -0500

    Renamed parse functions.

--- utilities/parse.py
+++ utilities/parse.py
@@ -25,31 +25,32 @@ class Ref(typing.Generic[T]):
         self.value: T = value
 
 
-def parse_data(data: bytes, index: Ref[int], DataType: typing.Type[T]) -> T:
+def parse_from_bytes(
+        data: bytes, index: Ref[int], DataType: typing.Type[T]) -> T:
     if inspect.isclass(DataType):
         if issubclass(DataType, bt.BasicType):
             value = du.unpack(data, index.value, DataType)
             index.value += bt.BASIC_TYPE_FORMAT[DataType].size
             return value  # type: ignore
         if issubclass(DataType, pk.EventPacket):
-            return DataType(parse_data(data, index, field.type) for
+            return DataType(parse_from_bytes(data, index, field.type) for
                             field in dataclasses.fields(DataType))  # type: ignore
     if typing.get_origin(DataType) is tuple:
-        return tuple(parse_data(data, index, e) for
+        return tuple(parse_from_bytes(data, index, e) for
                      e in typing.get_args(DataType))  # type: ignore
     if DataType in GameData:
-        return DataType(*[parse_data(data, index, field.type) for
+        return DataType(*[parse_from_bytes(data, index, field.type) for
                         field in dataclasses.fields(DataType)])
     raise ValueError(
         f'Unexpected type received by parse_data: {DataType}.')
 
 
-def parse(data: bytes) -> pk.Packet:
+def parse_packet(data: bytes) -> pk.Packet:
     PacketType = const.PACKET_TYPE[pu.get_packet_id(data)]  # type: ignore
     if PacketType is pk.EventPacket:
-        event_code = du.to_string(parse_data(
+        event_code = du.to_string(parse_from_bytes(
             data, Ref(pk.PACKET_HEADER_LENGTH), pk.EventCode))
         PacketType = const.EVENT_DETAILS_TYPE[event_code]
     index = Ref[int](0)
-    return PacketType(*[parse_data(data, index, field.type) for
+    return PacketType(*[parse_from_bytes(data, index, field.type) for
                       field in dataclasses.fields(PacketType)])  # type: ignore
